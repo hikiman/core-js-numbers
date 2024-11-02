bdc3b72297bd939ecfba56f9ff59b6f1560ceaba
Aleksei Denisov <tech.denisov@gmail.com>
2024-11-02 10:52:25 +0400

    feat: add getSumOfDigits function implementation

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -308,8 +308,15 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  let sum = 0;
+  let count = num;
+
+  while (count !== 0) {
+    sum += count % 10;
+    count = Math.floor(count / 10);
+  }
+  return sum;
 }
 
 /**
